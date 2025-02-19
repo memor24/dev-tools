4ed1577f8d94db81fc409b27d928bb0e8d497259
memor24 <you@example.com>
2025-02-19 06:12:33 -0500

    update repo

---
+++ GitHub/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.token
+}
